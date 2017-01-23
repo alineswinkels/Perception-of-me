cf764122bcf63e4a3bee9d0326f2bb720b747c7e
aline <aline.swinkels@hotmail.com>
2017-01-23 17:43:36 +0100

    groetjes mij

--- visualize.js
+++ visualize.js
@@ -84,29 +84,37 @@
 var answers = {};
 var count = 1;
 document.addEventListener("click", function(){
-  if (count < 21){
+  if (count < 22){
     answers["question_" + count] = {
-      self: Math.floor(Math.random() * 5) + 1,
+      self: 5,
       friend: Math.floor(Math.random() * 5) + 1
     }
   }
   console.log(answers);
   count++;
+
+
   draw();
+  draw2();
+
 });
 
 function getCoordinates(key, score){
-  var radius = 700;
+  var canvasWidth = parseInt($("canvas").css('width'));
+  var nrOptions = 5;
+  var nrQuestions = 20;
+  var degrees = 360 / nrQuestions;
+  var breaks = nrOptions * 2;
   var id = parseInt(key - 1);
-  var angle1 = id * 18;
+  var angle1 = id * degrees;
   var angle2 = 90;
   var angle3 = 180 - angle1 - angle2;
-  var long = score * (radius / 10);
+  var long = score * (canvasWidth / breaks);
   var down = (long * (Math.sin(angle3 * Math.PI / 180.0))) / (Math.sin(angle2 * Math.PI / 180.0));
   var up = (long * (Math.sin(angle1 * Math.PI / 180.0))) / (Math.sin(angle2 * Math.PI / 180.0));
 
-  var coordinateX = (radius / 2) + down;
-  var coordinateY = (radius / 2) + up;
+  var coordinateX = (canvasWidth / 2) + down;
+  var coordinateY = (canvasWidth / 2) + up;
 
   var coordinates = {
     x: coordinateX,
@@ -117,13 +125,16 @@ function getCoordinates(key, score){
 }
 
 function draw(){
-  var canvas = document.getElementById('datacanvas');
-  if (canvas.getContext){
-    var ctx = canvas.getContext('2d');
+  var canvasSelf = document.getElementById('self');
+  if (canvasSelf.getContext){
+    var ctx = canvasSelf.getContext('2d');
     var firstinput = answers.question_1.self;
     var firstcoordinates = getCoordinates(1, firstinput);
     console.log(firstcoordinates);
     ctx.moveTo(firstcoordinates.x, firstcoordinates.y);
+    ctx.strokeStyle="#F12F79";
+    ctx.fillStyle="rgba(241,47,121,0.1)";
+    ctx.lineWidth=5;
 
     for (var answer in answers){
       var key = parseInt(answer.split('_').pop());
@@ -137,6 +148,36 @@ function draw(){
 
       ctx.lineTo(coordinateX, coordinateY);
     }
+    ctx.stroke();
+    ctx.fill();
+  }
+}
+
+function draw2(){
+  var canvasFriend = document.getElementById('friend');
+  if (canvasFriend.getContext){
+    var ctx = canvasFriend.getContext('2d');
+    var firstinput = answers.question_1.friend;
+    var firstcoordinates = getCoordinates(1, firstinput);
+    console.log(firstcoordinates);
+    ctx.moveTo(firstcoordinates.x, firstcoordinates.y);
+    ctx.strokeStyle="rgb(123,127,130)";
+    ctx.fillStyle="rgba(123,127,130,0.1)";
+    ctx.lineWidth=5;
+
+    for (var answer in answers){
+      var key = parseInt(answer.split('_').pop());
+      var input = answers[answer].friend;
+      var coordinates = getCoordinates(key, input);
+      var coordinateX = coordinates.x;
+      var coordinateY = coordinates.y;
+      console.log("X: " + coordinateX + " Y: " + coordinateY);
+      // var coordinateX = coordinates[answer][input].x;
+      // var coordinateY = coordinates[answer][input].y;
+
+      ctx.lineTo(coordinateX, coordinateY);
+    }
+    ctx.stroke();
     ctx.fill();
   }
 }
